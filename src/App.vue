4de09641ca7dac13d489be592f11b29436140b18
Zeng3531 <10725296+zeng3531@user.noreply.gitee.com>
2022-10-19 16:39:42 +0800

    创建模板

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="app">
+        <h1>hello vue2+vite</h1>
+        <van-button type="primary">主要按钮</van-button>
+        <van-button type="info">信息按钮</van-button>
+        <van-button type="default">默认按钮</van-button>
+        <van-button type="warning">警告按钮</van-button>
+        <van-button type="danger">危险按钮</van-button>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link> |
+        <router-link to="/test">test</router-link>
+        <router-view />
+
+
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style lang="scss">
+.App {
+    ul {
+        li {
+            list-style: none;
+        }
+    }
+}
+</style>
+
